88de196ae4197ebeb637b9f11ac93b38325f046b
Alan Lima <alan161094@hotmail.com>
2025-03-01 23:42:59 -0300

    clear imports UserDTO

--- src/main/java/com/picpay/domain/user/UserDTO.java
+++ src/main/java/com/picpay/domain/user/UserDTO.java
@@ -1,11 +1,7 @@
 package com.picpay.domain.user;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 
 import java.math.BigDecimal;
-
-public record UserDTO( String firstName, String lastName, String document, String email, String password, BigDecimal balance,UserType userType) {
+public record UserDTO( Long id, String firstName, String lastName, String document, String email, String password, BigDecimal balance,UserType userType) {
 
 }
