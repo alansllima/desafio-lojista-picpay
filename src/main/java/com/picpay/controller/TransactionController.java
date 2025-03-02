3f8abafb19d2a489cf14346a1d679e469a18954f
Alan Lima <alan161094@hotmail.com>
2025-03-01 21:19:28 -0300

    swagger annotations tags

--- src/main/java/com/picpay/controller/TransactionController.java
+++ src/main/java/com/picpay/controller/TransactionController.java
@@ -5,20 +5,20 @@ import com.picpay.domain.transaction.TransactionDTO;
 import com.picpay.repository.TransactionRepository;
 import com.picpay.service.TransactionService;
 import com.picpay.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/transaction")
+@Tag(name = "Exemplo transactions",description = "description teste")
 public class TransactionController {
     @Autowired
     private UserService userService;
@@ -26,10 +26,12 @@ public class TransactionController {
     private TransactionService transactionService;
 
     @GetMapping("/transactions")
+    @Operation(summary = "get all Transactions")
     public ResponseEntity<List<Transaction>> getTransactions(){
         return ResponseEntity.ok(transactionService.getTransactions());
     }
     @PostMapping()
+    @Operation(summary = "send new Transaction")
     public ResponseEntity<TransactionDTO> createTransaction(@RequestBody @Validated TransactionDTO transactionDTO){
         TransactionDTO transaction = transactionService.createTransaction(transactionDTO);
         return ResponseEntity.ok(transaction);
--- src/main/java/com/picpay/controller/UserController.java
+++ src/main/java/com/picpay/controller/UserController.java
@@ -3,6 +3,8 @@ package com.picpay.controller;
 import com.picpay.domain.user.User;
 import com.picpay.domain.user.UserDTO;
 import com.picpay.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.models.annotations.OpenAPI31;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,16 +15,18 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
+@Tag(name = "User")
 public class UserController {
     @Autowired
     private UserService userService;
 
     @GetMapping("/users")
+    @Operation(summary = "get all Users")
     public ResponseEntity<List<User>> getUsers(){
-
         return ResponseEntity.ok(userService.findAll());
     }
     @PostMapping("/register")
+    @Operation(summary = "create new User")
     public ResponseEntity<UserDTO> createUser(@RequestBody @Validated UserDTO user){
         UserDTO userCreated = userService.createUser(user);
         return ResponseEntity.ok(userCreated);
