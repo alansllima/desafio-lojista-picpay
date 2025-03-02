a41a7e4953ca20f8118b7bbd07f8293a01faac71
Alan Lima <alan161094@hotmail.com>
2025-03-01 23:43:51 -0300

    User class with lombok getter and setters

--- src/main/java/com/picpay/domain/user/User.java
+++ src/main/java/com/picpay/domain/user/User.java
@@ -15,14 +15,15 @@ import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
 @Entity(name ="users")
 @Table(name = "users")
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class User {
-
     @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
@@ -49,70 +50,8 @@ public class User {
         this.balance = balance;
         this.userType = userType;
     }
-    public User(){}
 
-
-    public String getFirstName() {
-        return firstName;
+    public User() {
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getDocument() {
-        return document;
-    }
-
-    public void setDocument(String document) {
-        this.document = document;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
-    }
-
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
-
-    public UserType getUserType() {
-        return userType;
-    }
-
-    public void setUserType(UserType userType) {
-        this.userType = userType;
-    }
-
-    public Set<Transaction> getTransactions() {
-        return transactions;
-    }
-
-    public void setTransactions(Set<Transaction> transactions) {
-        this.transactions = transactions;
-    }
 }
